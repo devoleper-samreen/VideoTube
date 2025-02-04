f67535a22c5bea1e37b4eec77867e6682f9e440b
devoleper-samreen <samreenmalik52292@gmail.com>
2025-02-04 12:57:30 +0530

    add passport authentication setup and related middleware

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import dotenv from 'dotenv';
 import connectDB from "./DB/dbConnect.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import passport from "passport"
 
 dotenv.config(
     {
@@ -10,8 +13,19 @@ dotenv.config(
 
 const app = express();
 const port = process.env.PORT || 5000;
+const corsOptions = {
+    origin: process.env.FRONTEND_HOST,
+    credential: true,
+    optionSuccesStatus: 200
+}
 
-connectDB();
+//middlewares
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+app.use(passport.initialize())
+
+//connectDB();
 
 app.listen(port, () => {
     console.log(`App is running on : http://localhost:${port}`);
