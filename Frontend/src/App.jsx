2db8d0006b41a15b9434ae993e16661f8b428e62
ruchap24 <patilrucha991@gmail.com>
2025-02-04 14:42:22 +0530

    Add Sidebar component and integrate it into App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,15 @@
+import ListItems from './components/ListItems';
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar';
 
 //rfce
 function App() {
   return (
-    <div><Navbar /></div>
+    <div>
+      <Navbar />
+      <ListItems />
+      <Sidebar />
+      </div>
   );
 }
 
