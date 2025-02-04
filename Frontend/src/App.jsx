a1c8206f012d1ae693bba987793663dd2bfdd6fc
ruchap24 <patilrucha991@gmail.com>
2025-02-04 14:56:51 +0530

    Refactor App component to remove merge conflict artifacts and clean up code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 import ListItems from './components/ListItems';
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar';
-=======
-import Navbar from './components/Navbar'
->>>>>>> a4481b755087f9dfd97f74a6ac0bcc162ee47753
 
-//rfce
 function App() {
   return (
-<<<<<<< HEAD
     <div>
       <Navbar />
       <ListItems />
       <Sidebar />
       </div>
-=======
-    <div><Navbar /></div>
->>>>>>> a4481b755087f9dfd97f74a6ac0bcc162ee47753
   );
 }
 
-export default App
+export default App;
