f74fdc1a73c2ac4472209fc8ff658c1a16aa1179
ruchap24 <patilrucha991@gmail.com>
2025-02-05 16:47:19 +0530

    Add axios dependency and create rapidapi utility for API calls

---
+++ Frontend/src/utils/rapidapi.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_KEY=import.meta.env.VITE_VIDEOTUBE_API_KEY;
+
+const BASE_URL = 'https://youtube138.p.rapidapi.com';
+
+const options = {
+	method: 'GET',
+	hostname: 'youtube138.p.rapidapi.com',
+	headers: {
+		'x-rapidapi-key': 'API_KEY',
+		'x-rapidapi-host': 'youtube138.p.rapidapi.com'
+	}
+};
+
+export const fetchData = async (url) => {
+	try {
+		const {data}=await axios.get(`${BASE_URL}${url}`, options);
+	}
+	catch (error) {
+		console.error(error);
+	}
+}
