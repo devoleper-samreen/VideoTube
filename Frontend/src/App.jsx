a01ace9880d1e468b90f69f35f8acca49e17fe8d
ruchap24 <patilrucha991@gmail.com>
2025-02-05 17:02:38 +0530

    Add AuthProvider context for authentication state management

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import ListItems from './components/ListItems';
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar';
+import {useAuth} from './context/AuthProvider';
 
 function App() {
+  const {loading, data}=useAuth();
+  console.log(loading);
+  console.log(data);
   return (
     <div>
       <Navbar />
