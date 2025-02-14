32e3fdcdcb50610e49c2a67fdf8eb5d15a36f19f
devoleper-samreen <samreenmalik52292@gmail.com>
2025-02-14 17:10:06 +0530

    Add video retrieval and management routes for updating and deleting videos

--- Backend/src/routes/videoRoute.js
+++ Backend/src/routes/videoRoute.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { publishVideo } from '../controllers/video.js';
+import {
+    publishVideo,
+    getVideoById,
+    getAllVideos,
+    deleteVideo,
+    updateVideo
+} from '../controllers/video.js';
 import { verifyToken } from '../middelwares/verifyJWT.js';
 import { upload } from '../middelwares/multur.js';
 
@@ -17,5 +23,17 @@ router.post('/', upload.fields([
     },
 
 ]), publishVideo)
+
+router.get('/:videoId', getVideoById)
+router.get('/:userId', getAllVideos)
+router.delete('/:videoId', deleteVideo)
+router.patch('/:videoId', upload.fields([
+    {
+        name: "thumbnail",
+        maxCount: 1,
+    },
+
+]), updateVideo)
+
 
 export default router;
