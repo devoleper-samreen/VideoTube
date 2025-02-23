120fbea7f9b82861642520c296153026df39301a
devoleper-samreen <samreenmalik52292@gmail.com>
2025-02-23 16:05:44 +0530

    DELETE UNWANTED THINGS

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search/:searchQuery" element={<Search />} />
         <Route path="/playing" element={<PlayingVideo />} />
+
       </Routes>
     </div>
   );
